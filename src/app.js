dc5ab61f3c14da9c2eb4dca848433a0b686020be
NataliaSacias <nsacias@gmail.com>
2021-04-16 19:58:03 +0000

    imprime en el html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //   const nombres = ["pepe", "luna", "paz", "meme", "luz"];
+  //   const paises = ["mx", "ar", "uy", "cl"];
+
+  const obtenerRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+  document.write(
+    `${obtenerRandom(who)} ${obtenerRandom(action)} ${obtenerRandom(
+      action
+    )} ${obtenerRandom(what)} ${obtenerRandom(when)}`
+  );
+  //  console.log("Hello Rigo from the console!");
 };
